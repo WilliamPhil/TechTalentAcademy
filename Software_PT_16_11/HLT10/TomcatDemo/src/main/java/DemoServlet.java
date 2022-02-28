ecb05b6e419abdd66665ffdc39d1ddb557b57376
WilliamPhil <46234049+WilliamPhil@users.noreply.github.com>
2022-02-28 21:22:19 +0000

    Hlt10 complete

---
+++ Software_PT_16_11/HLT10/TomcatDemo/src/main/java/DemoServlet.java
@@ -0,0 +1,58 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DemoServlet
+ */
+@WebServlet("/DemoServlet")
+public class DemoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DemoServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		try {
+			
+			PrintWriter write = response.getWriter();
+			response.setContentType("text/html");
+			
+			String name = request.getParameter("uname");
+			write.print("Hi " + name);
+			
+			String pass = request.getParameter("pass");
+			write.print("Your password is: " + pass);
+			
+			
+		}
+		catch(Exception e) {
+			System.out.println("error : " + e);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
